e8c484711549f17d7d281699f6aa01d1a24e03fa
Jonathan Bridge <jvbridge@gmail.com>
2022-06-16 11:02:28 -0700

    better error handling for authorization

--- src/server/util/auth.ts
+++ src/server/util/auth.ts
@@ -1,7 +1,8 @@
 import * as jwt from 'jsonwebtoken';
-// TODO: .env variables
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+
+// configuration for JWT
+const secret = process.env.SECRET || 'mysecretssshhhhhhh';
+const expiration = process.env.EXPERATION || '2h';
 
 // user datatype for what we can expext a user to be
 type User = {
@@ -18,14 +19,22 @@ declare module 'jsonwebtoken' {
 
 // helper function to verify data
 export const userDataJwtPayload = (jwtToken: string): User | undefined => {
+  // try to decode the data
   try {
     const { data } = <jwt.userDataJwtPayload>(
       jwt.verify(jwtToken, secret, { maxAge: expiration })
     );
 
     return data;
-  } catch (error) {
-    console.log('Invalid web token passed through middleware');
+  } catch (error: any) {
+    // data not decoded
+
+    // we always hit this block if the web token doesn't exist
+    if (error.message == 'jwt must be provided')
+    return undefined;
+
+    // if there is a reason why, that's not a null token, lets get it
+    console.error(error);
     return undefined;
   }
 };
