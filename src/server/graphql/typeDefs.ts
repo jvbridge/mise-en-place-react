fe9533b526e70e753da2332fa401bf44d66e0011
Jonathan Bridge <jvbridge@gmail.com>
2022-06-15 17:06:03 -0700

    fixed capitolization

--- src/server/graphql/typeDefs.ts
+++ src/server/graphql/typeDefs.ts
@@ -1,25 +1,25 @@
-import { gql } from "apollo-server-express"
+import { gql } from 'apollo-server-express';
 
-const typedefs = gql`
-type User {
-  _id: ID
-  email: String
-  password: String
-}
+const typeDefs = gql`
+  type User {
+    _id: ID
+    email: String
+    password: String
+  }
 
-type Auth {
-  token: ID!
-  user: User
-}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-type Query {
-  me: User
-}
+  type Query {
+    me: User
+  }
 
-type Mutation{
-  addUser(email: String!, password: String!): Auth
-  login(email: String!, password: String!): Auth
-}
-`
+  type Mutation {
+    addUser(email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
 
-export default typedefs;
+export default typeDefs;
