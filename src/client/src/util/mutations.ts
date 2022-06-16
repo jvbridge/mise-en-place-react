911ace8cdadba307c4ceb4f3ddbedf1e6e30b059
Jonathan Bridge <jvbridge@gmail.com>
2022-06-16 11:36:36 -0700

    login mutation

--- src/client/src/util/mutations.ts
+++ src/client/src/util/mutations.ts
@@ -10,3 +10,14 @@ export const ADD_USER = gql`
     }
   }
 `
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      email
+    }
+  }
+}
+`
