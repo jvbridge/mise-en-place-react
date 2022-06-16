60c95f77743ac5a3b07b83a5939005adcc7ea7f5
Jonathan Bridge <jvbridge@gmail.com>
2022-06-16 10:47:02 -0700

    a few more comments

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,24 +4,29 @@ import path = require('path');
 import { authMiddleware } from './util/auth'; 
 import {typeDefs, resolvers} from './schema'
 import connection from './config/connection'
+import { DocumentNode } from 'graphql';
 
 // changing the reference for ease of remembering
 const db = connection;
 
 // the port we will be on will be 3001 or whatever the service we use has
 const PORT = process.env.Port || 3001;
+
+// create the express server
 const app = express();
 
-
+// create the graphql server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
+// basic middleware 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// redirect all gets in production to be handled by react router
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('/*', (req, res) => {
@@ -29,9 +34,15 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const start = async (typedefs: any, resolvers: any) => {
-  // start the server
+/**
+ * The start function for the server
+ * @param typedefs 
+ * @param resolvers 
+ */
+const start = async (typedefs: DocumentNode, resolvers: any) => {
+  // start the graphql server
   await server.start();
+
   // allow our app to be added
   server.applyMiddleware({ app });
 
@@ -46,4 +57,5 @@ const start = async (typedefs: any, resolvers: any) => {
   });
 };
 
+// set up complete time to run the server
 start(typeDefs, resolvers);
