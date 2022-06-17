f9aba94ac590ad5c2a3f3bb6cc52e03e51502e13
Jonathan Bridge <jvbridge@gmail.com>
2022-06-17 15:19:08 -0700

    installing react router

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -9,7 +10,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
 
+      </Router>
     </ApolloProvider>
   );
 }
