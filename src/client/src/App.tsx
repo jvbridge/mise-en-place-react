6fd2bcbb82b79e16a9f376e2039162342daeda0c
Jonathan Bridge <jvbridge@gmail.com>
2022-06-17 21:32:26 -0700

    a few comments

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+// routing and backend connection
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// pages
 import Homepage from './pages/Homepage';
 import Calendar from './pages/Calendar';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/notFound';
 
+// apollo client initialization
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache()
 });
 
+// main app
 function App() {
   return (
     <ApolloProvider client={client}>
