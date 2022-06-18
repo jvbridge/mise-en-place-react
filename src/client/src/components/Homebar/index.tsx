225dbf6f3b325d6b1bc665465d4862131a90672f
Jonathan Bridge <jvbridge@gmail.com>
2022-06-17 22:22:18 -0700

    removed extraineous hrefs

--- src/client/src/components/Homebar/index.tsx
+++ src/client/src/components/Homebar/index.tsx
@@ -2,32 +2,31 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import logo from './mise_en_place_transparent.png';
-function Homebar() {
+function Homebar(/*{ loggedIn }: { loggedIn: boolean }*/) {
   return (
     <Navbar className="justify-content-center">
       <img src={logo} alt="" width="100" height="80" id="logo" />
       <Container>
         <Nav>
           <LinkContainer to={'/Home'}>
-            <Nav.Link href="/Home">Home</Nav.Link>
+            <Nav.Link>Home</Nav.Link>
           </LinkContainer>
           <LinkContainer to={'/Dashboard'}>
             <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
           </LinkContainer>
           <LinkContainer to={'/Calendar'}>
-            <Nav.Link href="/Calendar">Calendar</Nav.Link>
+            <Nav.Link>Calendar</Nav.Link>
           </LinkContainer>
           <LinkContainer to={'/Upcoming'}>
-            <Nav.Link href="/Upcoming">Upcoming</Nav.Link>
+            <Nav.Link>Upcoming</Nav.Link>
           </LinkContainer>
           <LinkContainer to={'/Repeated'}>
-            <Nav.Link href="/Repeated">Repeated</Nav.Link>
+            <Nav.Link>Repeated</Nav.Link>
           </LinkContainer>
           <LinkContainer to={'/Checklist'}>
-            <Nav.Link href="/Checklist">Checklist</Nav.Link>
+            <Nav.Link>Checklist</Nav.Link>
           </LinkContainer>
-          {/*             
-Need conditional for logged in
+          {/* Need conditional for logged in
             <li class="nav-item">
                 <a class="nav-link" id="logout" href="/login">Logout</a>
             </li> */}
