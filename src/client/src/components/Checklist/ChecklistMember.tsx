f9970fedba16cd5c13c77e2ec6fd86994bec12b0
Jonathan Bridge <jvbridge@gmail.com>
2022-06-20 11:50:38 -0700

    added buttons to the items

--- src/client/src/components/Checklist/ChecklistMember.tsx
+++ src/client/src/components/Checklist/ChecklistMember.tsx
@@ -1,4 +1,5 @@
-import { ListGroupItem } from 'react-bootstrap';
+import { useState } from 'react';
+import { ListGroupItem, ToggleButton } from 'react-bootstrap';
 
 export interface ChecklistItemProps {
   name: string;
@@ -6,10 +7,24 @@ export interface ChecklistItemProps {
   display: boolean;
 }
 
+// TODO: import values
+
 function ChecklistMember(props: ChecklistItemProps) {
   const { name, done } = props;
+  const [checked, setChecked] = useState(done);
   return (
-    <ListGroupItem className="list-group-item to-do-item">{name}</ListGroupItem>
+    <ListGroupItem className="to-do-item">
+      <ToggleButton
+        type="checkbox"
+        variant=""
+        checked={checked}
+        value="1"
+        onChange={(e) => setChecked(e.currentTarget.checked)}
+      >
+        Done
+      </ToggleButton>{' '}
+      {name}
+    </ListGroupItem>
   );
 }
 
