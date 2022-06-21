38a63a96faba8a72336ec48d635a53801659eba1
Jonathan Bridge <jvbridge@gmail.com>
2022-06-21 11:24:59 -0700

    redid structure of the app makes todos everywhere

--- src/client/src/pages/Dashboard/index.tsx
+++ src/client/src/pages/Dashboard/index.tsx
@@ -1,30 +1,7 @@
-import Checklist from '../../components/Checklist';
-import { ChecklistItem } from '../../components/Checklist';
-
 function Dashboard() {
-  const items: ChecklistItem[] = [{ name: 'test Item', done: true }];
-
-  const missedItems: ChecklistItem[] = [];
   return (
     <>
       <div>Dashboard</div>
-      <div className="container">
-        <div className="row">
-          <div className="col-9">filer space</div>
-          <div className="col-3">
-            <Checklist
-              name="Passed in name"
-              checklistItems={items}
-              displayList={false}
-            />
-            <Checklist
-              name="Missed Todos:"
-              checklistItems={missedItems}
-              displayList={true}
-            />
-          </div>
-        </div>
-      </div>
     </>
   );
 }
