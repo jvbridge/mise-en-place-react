e1a8d2dc54b44a814f09a2215aa59fec55ea0fd3
Jonathan Bridge <jvbridge@gmail.com>
2022-06-21 11:32:33 -0700

    removed extra todolists

--- src/client/src/pages/Calendar/index.tsx
+++ src/client/src/pages/Calendar/index.tsx
@@ -7,7 +7,7 @@ function Calendar() {
 
       <div className="container container-fluid">
         <div className="row">
-          <div className="col-9 main-container">
+          <div className="main-container">
             <div className="card calendar-card" style={{ width: '100%' }}>
               <div className="card-body">
                 <div className="card-header calendar-header card-title">
@@ -20,37 +20,6 @@ function Calendar() {
                   <button className="btn addToCal-btn">Add an Event</button>
                 </a>
               </div>
-            </div>
-          </div>
-          {/* TODO: checklist component */}
-          <div className="col-3">
-            <div className="card to-do-card" style={{ width: '100%' }}>
-              <div className="card-header to-do-card-header">
-                To Do:
-                <a href="checklist" id="plus">
-                  <i className="fa-solid fa-plus"></i>
-                </a>
-              </div>
-              <ul className="list-group list-group-flush">
-                {/* {{#each checklistItems as |item|}} */}
-                <li className="list-group-item to-do-item">
-                  checklist name here
-                </li>
-                {/* {{/each}} */}
-              </ul>
-            </div>
-
-            <div className="card to-do-card" style={{ width: '100%' }}>
-              <div className="card-header missed-card-header">
-                Missed To Dos:
-              </div>
-              <ul className="list-group list-group-flush">
-                {/* {{#each checklistItems as |item|}} */}
-                <li className="list-group-item missed-item">
-                  checklist name here
-                </li>
-                {/* {{/each}} */}
-              </ul>
             </div>
           </div>
         </div>
