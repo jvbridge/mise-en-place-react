1488af14722837aa49b217398d1dae114a2a23ab
Jonathan Bridge <jvbridge@gmail.com>
2022-06-28 12:04:44 -0700

    removal pre script

--- src/server/models/Checklist.ts
+++ src/server/models/Checklist.ts
@@ -1,4 +1,5 @@
 import { Schema, model, Document, Model, Types } from 'mongoose';
+import User from './User';
 
 export interface ChecklistItem {
   _id: Types.ObjectId;
@@ -23,6 +24,21 @@ const checklistSchema = new Schema<ChecklistDocument>({
     ref: 'User',
   },
   items: [],
+});
+
+// before removing a checklist make sure that it removes itself from the user
+checklistSchema.pre('remove', async function (next) {
+  // get the reference to the user
+  const parentUser = await User.findById(this.user);
+
+  // the checklist must have a parent user
+  if (!parentUser) throw new Error('could not find parent user');
+
+  // remove the current checklist from the parent's checklists
+  parentUser.checklists = parentUser.checklists.filter((list) => {
+    list._id != this._id;
+  });
+  await parentUser.save();
 });
 
 const Checklist: Model<ChecklistDocument> = model('Checklist', checklistSchema);
