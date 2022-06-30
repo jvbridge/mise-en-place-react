a39eda92be23045cdec96eefc8b8902ab28ce668
Jonathan Bridge <jvbridge@gmail.com>
2022-06-30 12:47:04 -0700

    imported old html from handlebars

--- src/client/src/pages/Checklist/index.tsx
+++ src/client/src/pages/Checklist/index.tsx
@@ -1,5 +1,65 @@
 function ChecklistPage() {
-  return <div>Checklist page</div>;
+  return (
+    <div className="row">
+      <div className="main-container">
+        <div className="card checklist-card" style={{ width: '100%' }}>
+          <div className="card-body">
+            <div className="card-header dash-header card-title">
+              <h5>
+                To Do List:
+                {/* ADD event listener to create input form */}
+                <button className="addCheck link-button plus">
+                  <i className="fa-solid fa-plus " aria-hidden="false"></i>
+                </button>
+              </h5>
+            </div>
+            <div className="checklist-form">
+              <div className="input-group mb-3" id="addCheck-form" hidden>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="New To Do List"
+                  aria-label="checklist-item"
+                  aria-describedby="button-addon2"
+                  id="addInput"
+                />
+                <button type="button" id="addon-btn">
+                  Add List
+                </button>
+              </div>
+
+              {/* Checklists go here
+            
+            {{#each checklists as |checklist|}}
+              <div
+                className="checklist-id-wrapper"
+                data-checklist-id="{{checklist.id}}"
+              >
+
+                <ul><h3 id="checklist-name">{{checklist.name}}</h3>
+                  {{#each checklist.checklist_items as |item|}}
+                    <input
+                      className="form-check-input checklist-item-checkbox"
+                      type="checkbox"
+                      value=""
+                      id="checklist-item-{{item.id}}"
+                      {{#if item.is_complete}}checked{{/if}}
+                    />
+                    <label className="form-check-label" for="flexCheckChecked">{{item.description}}
+                    </label>
+                    <br />
+                  {{/each}}
+                </ul>
+              </div>
+
+              <button className="add-item-btn" type="button" id="addon-btn-{{checklist.id}}">Add Item</button>
+            {{/each}} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChecklistPage;
