dbced4b6612e19f169e1428b3d9282e80ed44bd5
Jonathan Bridge <jvbridge@gmail.com>
2022-07-05 13:04:25 -0700

    formSubmission for making new items

--- src/client/src/components/Checklist/index.tsx
+++ src/client/src/components/Checklist/index.tsx
@@ -1,4 +1,5 @@
-import { Card, ListGroup } from 'react-bootstrap';
+import React, { FormEvent, useState } from 'react';
+import { Card, ListGroup, Form, FormControl } from 'react-bootstrap';
 import ChecklistMember from './ChecklistMember';
 
 export interface ChecklistProps {
@@ -15,12 +16,39 @@ export interface ChecklistItem {
 }
 
 function Checklist({ checklistItems, name, displayList, id }: ChecklistProps) {
-  // conditional rendering for adding an item to the list
+  // usestate for adding new items to the list
+  const [newItem, setNewItem] = useState({ name: '', hidden: true });
+
+  // submission for handling a new item on the checklists
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log('submitting: ', newItem.name);
+    setNewItem({ name: '', hidden: true });
+  };
+
+  // handler that propagates the value to the usestate
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setNewItem({
+      hidden: newItem.hidden,
+      name: value,
+    });
+  };
+
+  // conditional rendering for adding an item to the list (for display lists)
   let addItem;
 
   if (!displayList) {
     addItem = (
-      <button className="link-button plus">
+      <button
+        className="link-button plus"
+        onClick={() => {
+          setNewItem({
+            name: newItem.name,
+            hidden: !newItem.hidden,
+          });
+        }}
+      >
         <i className="fa-solid fa-plus"></i>
       </button>
     );
@@ -34,11 +62,29 @@ function Checklist({ checklistItems, name, displayList, id }: ChecklistProps) {
           {addItem}
         </div>
       </Card.Header>
+      <Form
+        onSubmit={handleSubmit}
+        className="input-group"
+        hidden={newItem.hidden}
+      >
+        <FormControl
+          type="text"
+          placeholder="New Checklist Item"
+          aria-label="checklist-item"
+          aria-describedby="button-addon2"
+          onChange={handleChange}
+          value={newItem.name}
+        />
+        <button type="button" className="addon-btn" onClick={handleSubmit}>
+          Add Item
+        </button>
+      </Form>
       <ListGroup variant="flush">
         {checklistItems.length ? (
-          checklistItems.map((item) => {
+          checklistItems.map((item, index) => {
             return (
               <ChecklistMember
+                key={`List ${id}, item ${index}`}
                 id={item._id}
                 name={item.name}
                 done={item.done}
